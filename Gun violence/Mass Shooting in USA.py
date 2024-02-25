0620043a8631c863285af3f239f1e268da0c226b
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2024-02-25 17:54:50 +0600

    Update Mass Shooting in USA.py

--- Gun violence/Mass Shooting in USA.py
+++ Gun violence/Mass Shooting in USA.py
@@ -6,7 +6,14 @@ import pandas as pd
 
 df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\Data visualization\Mother Jones - Mass Shootings Database, 1982 - 2023 - Sheet1.csv")
 
+df[['city','state']]=df['location'].str.split(', ',expand=True)
 
 print(df)
 
 
+temp=df[['year','case']]
+consumptionStyle=temp.groupby('year',as_index=False).count()
+
+
+print(consumptionStyle)
+sb.barplot(data=consumptionStyle, x='year',y='case')
