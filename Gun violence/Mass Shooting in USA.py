a9c4220349309f64069057eb662c080556cfd828
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2024-02-25 11:28:38 +0600

    Update Mass Shooting in USA.py

--- Gun violence/Mass Shooting in USA.py
+++ Gun violence/Mass Shooting in USA.py
@@ -1,1 +1,12 @@
 
+import seaborn as sb
+import matplotlib as plt
+import pandas as pd
+
+
+df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\Data visualization\Mother Jones - Mass Shootings Database, 1982 - 2023 - Sheet1.csv")
+
+
+print(df)
+
+
