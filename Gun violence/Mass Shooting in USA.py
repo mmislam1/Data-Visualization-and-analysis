6628881d221dcda0ae37a00adc8d89f79897a5ad
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2024-02-25 20:57:20 +0600

    Update Mass Shooting in USA.py

--- Gun violence/Mass Shooting in USA.py
+++ Gun violence/Mass Shooting in USA.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 import seaborn as sb
 import matplotlib as plt
@@ -6,17 +11,21 @@ import pandas as pd
 
 df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\Data visualization\Mother Jones - Mass Shootings Database, 1982 - 2023 - Sheet1.csv")
 
+
 df[['city','state']]=df['location'].str.split(', ',expand=True)
 
 print(df)
 
 
 temp=df[['year','case']]
-consumptionStyle=temp.groupby('year',as_index=False).count()
+incidentsPerYear=temp.groupby('year',as_index=False).count()
 
 
-print(consumptionStyle)
-sb.lineplot(data=consumptionStyle, x='year',y='case')
+print(incidentsPerYear)
+sb.lineplot(data=incidentsPerYear, x='year',y='case').set(title='Incidents Per Year')
+
+
+# In[4]:
 
 
 temp=df[['state','case']]
@@ -25,4 +34,23 @@ shootingPerState=temp.groupby('state',as_index=False).count()
 
 print(shootingPerState)
 sb.set(rc = {'figure.figsize':(39,4)})
-sb.barplot(data=shootingPerState, x='state',y='case')
+sb.barplot(data=shootingPerState, x='state',y='case').set(title='Incidents Per State')
+
+
+# In[3]:
+
+
+temp=df[['age_of_shooter','case']]
+AgeOfShooter=temp.groupby('age_of_shooter',as_index=False).count()
+
+
+print(AgeOfShooter)
+sb.set(rc = {'figure.figsize':(39,4)})
+sb.barplot(data=AgeOfShooter, x='age_of_shooter',y='case').set(title='Age Of Shooter')
+
+
+# In[ ]:
+
+
+
+
