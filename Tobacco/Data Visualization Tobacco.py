c9d303516ccb5ae3112dc26c35581aaf8e4de862
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2024-02-25 11:14:20 +0600

    Update Data Visualization Tobacco.py

--- Tobacco/Data Visualization Tobacco.py
+++ Tobacco/Data Visualization Tobacco.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[2]:
 
 
 import seaborn as sb
@@ -9,7 +9,7 @@ import matplotlib as plt
 import pandas as pd
 
 
-df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\Data visualization\Adult_Tobacco_Consumption_In_The_U.S.__2000-Present.csv",index_col=False)
+df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\Data visualization\Data Visualization Tobacco\Adult_Tobacco_Consumption_In_The_U.S.__2000-Present.csv",index_col=False)
 
 
 print(df)
@@ -17,7 +17,7 @@ print(df)
 
 
 
-# In[32]:
+# In[3]:
 
 
 lp=df[["Total Per Capita",'Year']]
@@ -27,10 +27,23 @@ lp=df[["Total Per Capita",'Year']]
 sb.lineplot(x='Year',y='Total Per Capita',data=lp)
 
 
-# In[29]:
+# In[4]:
 
 
+sb.regplot(x='Year',y='Total Per Capita',data=lp)
 
+
+# In[7]:
+
+
+temp=df[["Measure","Total Per Capita"]]
+
+
+consumptionStyle=temp.groupby('Measure',as_index=False).sum()
+
+
+print(consumptionStyle)
+sb.barplot(data=consumptionStyle, x='Measure',y='Total Per Capita')
 
 
 # In[ ]:
